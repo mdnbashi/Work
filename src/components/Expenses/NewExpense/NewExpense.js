efc674453005fa91a3385f80883546978ab38e44
Masaru0217 <m7112252@gmial.com>
2022-12-04 22:42:44 +0900

    add:ExpaenseForm

---
+++ src/components/Expenses/NewExpense/NewExpense.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = () => {
+  return (
+    <duv className="new-expense">
+      <ExpenseForm />
+    </duv>
+  );
+};
+
+export default NewExpense;
